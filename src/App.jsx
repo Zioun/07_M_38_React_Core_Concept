8c1916b28f79dad8253a401de8c6815e96ee5861
Zioun <jionkhan0@gmail.com>
2024-03-08 07:56:50 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, corporis unde. Dolores aliquid exercitationem voluptatem. Ex nesciunt doloribus quia enim suscipit, quod excepturi iste officia explicabo, quas cumque quaerat voluptatem.</p>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Student></Student>
     </>
   )
 }
 
+function Person(){
+  const bonus = 25;
+  const money = 20;
+  const person = {name: "Sakib", age: 30}
+  return(
+    <h3>I am {person.name} age {person.age} money {money + bonus}</h3>
+  )
+}
+
+function Student(){
+  return (
+    <div>
+      <h1>I am a student</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum eos consequatur veritatis amet perferendis vitae.</p>
+    </div>
+  )
+}
 export default App
